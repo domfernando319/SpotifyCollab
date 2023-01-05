84a8a1f564ce7a0ae50418e19b2ff8828687102b
domfernando319 <domfernando319@gmail.com>
2023-01-05 00:16:34 -0500

    Set up inital api endpoints

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+# This is where you write all our endpoints e.g. /hello or /home
+
+# this will show on  the webpage when we hit this endpoint
+def main(request):
+    return HttpResponse("<h1>Hello<h1>")
+
