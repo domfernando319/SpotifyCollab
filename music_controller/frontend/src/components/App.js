1c7f38aac3627d2930b03ee16c669c99ce9a2073
domfernando319 <domfernando319@gmail.com>
2023-12-19 00:59:50 -0500

    Fixed some bugs

--- music_controller/frontend/src/components/App.js
+++ music_controller/frontend/src/components/App.js
@@ -1,22 +1,3 @@
-// import { render } from "react-dom";
-// import HomePage from "./HomePage";
-// import React, { Component } from 'react'
-
-// export default class App extends Component {
-//     constructor(props) {
-//         super(props);
-//     }
-//     render() {
-//         return(
-//             <div>
-//                 <HomePage />
-//             </div>
-//         )
-//     }
-// }
-
-// const appDiv = document.getElementById("app");
-// render(<App />, appDiv);
 import React from 'react';
 import HomePage from './HomePage';
 
