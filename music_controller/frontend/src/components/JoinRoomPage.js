00a272fd5c9a9854c5c04ed0463bdfc4e57f1608
domfernando319 <domfernando319@gmail.com>
2023-12-19 00:19:52 -0500

    Changed class components to functional. Get Room Details on REST api

--- music_controller/frontend/src/components/JoinRoomPage.js
+++ music_controller/frontend/src/components/JoinRoomPage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 
-export default class JoinRoomPage extends Component {
-    constructor(props) {
-        super(props);
-    }
-  render() {
-    return (
-      <div>Join a room</div>
-    )
-  }
+// export default class JoinRoomPage extends Component {
+//     constructor(props) {
+//         super(props);
+//     }
+//   render() {
+//     return (
+//       <div>Join a room</div>
+//     )
+//   }
+// }
+
+export default function JoinRoomPage() {
+  return (
+    <div>Join a Room</div>
+  )
 }
