867f229ce16b05641a88d579229da6309c3d53ee
domfernando319 <domfernando319@gmail.com>
2023-12-19 22:05:56 -0500

    Home Page automatically navigates to current browser session's room

--- music_controller/frontend/src/components/HomePage.js
+++ music_controller/frontend/src/components/HomePage.js
@@ -1,14 +1,61 @@
 import React, { Component } from 'react'
 import JoinRoomPage from './JoinRoomPage';
 import CreateRoomPage from './CreateRoomPage';
-import { BrowserRouter as Router, Routes, Link, Route, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Link, Route, Redirect, useNavigate, Navigate } from "react-router-dom";
 import Room from './Room';
+import {Grid, Button, ButtonGroup, Typography } from "@material-ui/core";
+import { useEffect, useState } from 'react';
+
+
 
 export default function HomePage() {
+
+  const [roomCode, setRoomCode] = useState(null)
+
+  useEffect(() => {
+    async function autoEnter() {
+        fetch('/api/user-in-room')
+            .then((response) => response.json())
+            .then((data) => {
+                setRoomCode(data.code);
+            })
+    };
+    autoEnter();
+    
+  },[]);
+
+  function renderHomePage() {
+    return (
+      <Grid container spacing={3}>
+        <Grid item xs={12} align="center">
+          {/* Title */}
+          <Typography variant="h3" compact="h3">
+            SpotifyCollab
+          </Typography>
+        </Grid>
+        <Grid item xs={12} align="center">
+          <ButtonGroup disableElevation variant="contained" color="primary">
+            <Button color ="primary" to='/join' component={Link}>
+              Join a Room
+            </Button>
+            <Button color ="secondary" to='/create' component={Link}>
+              Create a Room
+            </Button>
+          </ButtonGroup>
+        </Grid>
+      </Grid>
+    )
+  }
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<p>This is the home page</p>} />
+        <Route path="/" element={
+          roomCode ? (<Navigate replace to={`/room/${roomCode}`}/>) : renderHomePage()
+        }
+        />
+    
+
         <Route path="/join" element={<JoinRoomPage />} />
         <Route path="/create" element={<CreateRoomPage />} />
         <Route path="/room/:roomCode" element={<Room />} />
