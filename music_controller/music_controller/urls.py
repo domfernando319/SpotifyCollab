804ece7380272fd3f1923f7a2f87ca8d90524f2c
domfernando319 <domfernando319@gmail.com>
2023-12-28 18:29:54 -0500

    Added vercel app configuration

--- music_controller/music_controller/urls.py
+++ music_controller/music_controller/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,4 @@ urlpatterns = [
 ]
 # if I have 'domain.com/hello" after the slash will be sent to this file which dispatches endpoints
 
+# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
